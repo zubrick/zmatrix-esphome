3d5a4385f0868a21f6be774b8b3e4b7fce480d5d
Zubrick <zubrick@number6.ch>
2025-05-27 22:36:42 +0200

    remote components

--- components/zmatrix/__init__.py
+++ components/zmatrix/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.components import time
 
 CONF_WIDTH = "width"
 CONF_HEIGHT = "height"
